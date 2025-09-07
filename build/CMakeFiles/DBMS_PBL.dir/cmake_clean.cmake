91d40284047845ab89549974be671ab1395df870
Shashank <shashankvermayoyo@gmail.com>
2025-09-07 15:06:21 +0530

    Add new local folder content

---
+++ build/CMakeFiles/DBMS_PBL.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DBMS_PBL.dir/main.cpp.obj"
+  "CMakeFiles/DBMS_PBL.dir/main.cpp.obj.d"
+  "DBMS_PBL.exe"
+  "DBMS_PBL.exe.manifest"
+  "DBMS_PBL.pdb"
+  "libDBMS_PBL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DBMS_PBL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/DBMS_PBL.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DBMS_PBL.
+# This may be replaced when dependencies are built.
